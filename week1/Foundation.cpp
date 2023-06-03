ef8cd8701b6a9adbc05f2a52d9127e8e2efd4db5
bit-spite <spite@thebitstudio.com>
2023-06-03 15:59:48 +0700

    Foundation

---
+++ week1/Foundation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+  // varaibles
+  int age;  // Declaration of an integer variable named "age"
+
+  // data types
+  int age = 25;               // Declaration and initialization of an integer variable
+  double pi = 3.14159;        // Declaration and initialization of a double variable
+  char grade = 'A';           // Declaration and initialization of a character variable
+  bool isStudent = true;      // Declaration and initialization of a boolean variable
+  std::string message = "Hello, World!";  // Declaration and initialization of a string variable
+
+  // type modifiers
+  unsigned int positiveNumber = 10;  // Unsigned integer variable to store positive values only
+  long long bigNumber = 1234567890;  // Long integer variable to store a large range of values
+}
